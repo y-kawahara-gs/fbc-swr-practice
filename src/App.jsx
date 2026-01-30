bb76bca1d1291bc206332850081fb9424451dd18
y-kawahara-gs <y.kawahara@genbasupport.com>
2026-01-30 16:59:17 +0900

    fix: json.descriptionの取得を出力時に修正

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ const headers = { Accept: "application/json" };
 const fetcher = (url) =>
   fetch(url, { headers })
     .then((res) => res.json())
-    .then((json) => json.description);
 
 function App() {
   const { data, error, isLoading } = useSWR(url, fetcher);
   if (error) return <p className="App-header">Failed to load.</p>;
   if (isLoading) return <p className="App-header">Loading...</p>;
 
-  return <p className="App-header">{data && `Status : ${data}`}</p>;
+  return <p className="App-header">{data && `Status : ${data.description}`}</p>;
 }
 
 export default App;
